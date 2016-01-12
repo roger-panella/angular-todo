69e9a592d9ca232477e1b0283306aa4e2b54089c
Roger Panella <rogerpanella@gmail.com>
2016-01-11 23:21:30 -0600

    added controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
-angular.module("todoListApp",[]);
+angular.module("todoListApp",[])
+
+.controller('mainCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log('hello there.  This is the helloWorld controller function in the main controller');
+  };
+});
