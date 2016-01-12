498073af40282c63ae24d376d16972d8adc606fe
Roger Panella <rogerpanella@gmail.com>
2016-01-12 09:01:36 -0600

    added new controller functions

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,9 @@ angular.module("todoListApp",[])
     console.log('hello there.  This is the helloWorld controller function in the main controller');
   };
 });
+
+.controller('coolCtrl', function($scope) {
+  $scope.whoAmI = function() {
+      console.log("hello there, this is the coolCtrl function.");
+  };
+});
