d9362d73bd787c110a84eca71c11a57f5d580125
Roger Panella <rogerpanella@gmail.com>
2016-01-12 12:55:03 -0600

    scope lesson

--- scripts/app.js
+++ scripts/app.js
@@ -10,4 +10,14 @@ angular.module("todoListApp",[])
   $scope.whoAmI = function() {
       console.log("hello there, this is the coolCtrl function.");
   };
-});
+
+  $scope.helloWorld = function() {
+    console.log("This is not the main ctrl!");
+ };
+})
+
+ .controller('imASibling', function($scope) {
+    $scope.foobar = 1234;
+
+    // do other cool stuff!
+ });
