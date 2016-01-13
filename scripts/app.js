d92cf4669b5e9c12441d490db462db08ee46f7f8
Roger Panella <rogerpanella@gmail.com>
2016-01-12 20:24:35 -0600

    added directives started services

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,47 @@
 angular.module("todoListApp",[])
 
-.controller('mainCtrl', function($scope) {
-  $scope.helloWorld = function() {
-    console.log('hello there.  This is the helloWorld controller function in the main controller');
-  };
-})
-
-.controller('coolCtrl', function($scope) {
-  $scope.whoAmI = function() {
-      console.log("hello there, this is the coolCtrl function.");
+.controller('mainCtrl', function($scope, dataService) {
+  $scope.helloConsole = dataService.helloConsole;
+  $scope.learningNgChange = function() {
+    console.log("An input changed");
   };
 
-  $scope.helloWorld = function() {
-    console.log("This is not the main ctrl!");
- };
-})
 
- .controller('imASibling', function($scope) {
-    $scope.foobar = 1234;
+  $scope.todos = [
+    {"name": "clean the house"},
+    {"name": "water the dog"},
+    {"name": "feed the lawn"},
+    {"name": "pay dem bills"},
+    {"name": "run"},
+    {"name": "swim"}
+  ]
+
+ })
+
+ .service('dataService', function() {
+    this.helloConsole = function(){
+      console.log('This is the hello console service!');
+    }
+ });
+
+
+
+
+
+
+// })
+//
+// .controller('coolCtrl', function($scope) {
+//   $scope.whoAmI = function() {
+//       console.log("hello there, this is the coolCtrl function.");
+//   };
+//
+//   $scope.helloWorld = function() {
+//     console.log("This is not the main ctrl!");
+//  };
+// })
+
+ // .controller('imASibling', function($scope) {
+ //    $scope.foobar = 1234;
 
     // do other cool stuff!
- });
