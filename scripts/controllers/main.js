b44cd640aea64c696f36fe56d2831e0f5db95fcb
Roger Panella <rogerpanella@gmail.com>
2016-01-14 21:55:14 -0600

    more annotations

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -8,6 +8,10 @@ angular.module('todoListApp')
 // Typically, the function takes one parameter:  the $scope variable
 // $scope is an area of  operation for the controllers
 //    (controller only works in the part of the application you allow it to)
+
+// dataService is defining that service as a dependency in the mainCtrl controllers
+// Now, dataService's methods can be accessed as any other object could be.
+
 .controller('mainCtrl', function($scope, dataService) {
   $scope.addTodo = function() {
     var todo = {name: "This is a new todo."};
@@ -33,16 +37,25 @@ angular.module('todoListApp')
 
   $scope.helloWorld = dataService.helloWorld;
 
+// the callback function (function(response))is the callback function that happens after data is successfully returned from the request
+// it gets the response from the 'then' method in the data.js file (service)
   dataService.getTodos(function(response) {
       console.log(response.data);
       $scope.todos = response.data;
     });
 
+
+//  method to delete a todo (or other data)
+// This would be used to delete from the database (CRUD), which isn't done here since there is no db
+
+// $index is paramater given by angular in ng-repeats that gives you the current item number in the array being 'looped through'
   $scope.deleteTodo = function(todo, $index) {
     dataService.deleteTodo(todo);
+    //The splice deletes the current todo ($index - first argument).  The second argument is how many items to delete. So, here just one todo, the one clicked on.
     $scope.todos.splice($index, 1);
   };
 
+// Save todo.  Uses the saveTodo method from the dataService service
   $scope.saveTodos = function() {
     var filteredTodos = $scope.todos.filter(function(todo) {
       if(todo.edited){
--- scripts/services/data.js
+++ scripts/services/data.js
@@ -1,11 +1,35 @@
 'use strict';
 
+// Services allow you to create functions that can be used by multiple controllers.
+// By using 'dependency injection', a service can be required by a controller, which makes its methods available to that controllers
+// Dependency injection is how angular makes the same code available to multiple parts of the program
+// As long as controllers define the service as a dependency, multiple controllers can use one service.
+// The service that needs to be used in a controller is the second parameter, as 'dataService' in this controller:
+// .controller('mainCtrl', function($scope, dataService) {
+//   $scope.addTodo = function() {
+//     var todo = {name: "This is a new todo."};
+//     $scope.todos.unshift(todo);
+//   };
+
+// You can see in the 'main.js' file how the "getTodos" from the dataService is used within that controller.
+
 angular.module('todoListApp')
+
 .service('dataService', function($http) {
+  // 'this.helloWorld' makes it available as a method of the 'dataService' service
+  // This method of the service was an example from the video
   this.helloWorld = function() {
     console.log("This is the data service's method!!");
   };
 
+
+// This is a method of dataService that gets data, in this case from our own json.
+// $http is how you use the http provider, which is used to handle all http requests.
+// $http uses XMLHttpRequest or JSONP
+
+// This is a built in method (provider) to angular that makes getting data easy.
+// More info on data calls:  https://docs.angularjs.org/api/ng/service/$http
+// callback function happens when a successful response is received
   this.getTodos = function(callback){
     $http.get('mock/todos.json')
     .then(callback)
